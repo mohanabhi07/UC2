6b6ec967562aca18dd4f6d1fd84967e7040fc036
Mohan Y <142613187+mohanabhi07@users.noreply.github.com>
2025-02-07 18:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Middleware
 app.use(bodyParser.json());
